64ff1e690b460e34b43ae12be1cecd13131e48d5
olaf.goerlitz <olaf.goerlitz@d23ae19b-2206-6962-c3d4-7c8097259e2e>
2011-01-25 16:35:42 +0000

    extended evaluation

--- trunk/src/de/uni_koblenz/west/optimizer/rdf/BGPQueryModel.java
+++ trunk/src/de/uni_koblenz/west/optimizer/rdf/BGPQueryModel.java
@@ -136,18 +136,18 @@ public abstract class BGPQueryModel<P, F> implements QueryModel<BGPOperator<P, F
 		baseOperators = new ArrayList<BGPOperator<P,F>>();
 		Map<Set<Graph>, List<P>> graphSets = sourceFinder.findPlanSetsPerSource(getAllPatterns());
 		
-		// evaluation
-		Set<Graph> graphs = new HashSet<Graph>();
-		int planCount = 0;
-		for (Set<Graph> graphSet : graphSets.keySet()) {
-			graphs.addAll(graphSet);
-//			if (graphSet.size() == 1) {
-//				planCount++;
-//			} else {
-				planCount += graphSet.size() * graphSets.get(graphSet).size();
-//			}
-		}
-		System.out.println(planCount + " plans for " + graphs.size() + " graphs: " + graphs);
+//		// evaluation
+//		Set<Graph> graphs = new HashSet<Graph>();
+//		int planCount = 0;
+//		for (Set<Graph> graphSet : graphSets.keySet()) {
+//			graphs.addAll(graphSet);
+////			if (graphSet.size() == 1) {
+////				planCount++;
+////			} else {
+//				planCount += graphSet.size() * graphSets.get(graphSet).size();
+////			}
+//		}
+//		System.out.println(planCount + " plans for " + graphs.size() + " graphs: " + graphs);
 		
 		
 		for (Set<Graph> graphSet : graphSets.keySet()) {
