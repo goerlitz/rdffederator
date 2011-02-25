465815e9f6439c9f1a0d62361868e5e216bfbb43
goerlitz <olaf.goerlitz@gmail.com>
2011-02-25 11:35:59 +0000

    improved chart generation script

--- setup/results/eval_selection_and_plot_results.sh
+++ setup/results/eval_selection_and_plot_results.sh
@@ -6,15 +6,30 @@ for jar in `ls ../../lib/*.jar`; do
   path=$path:$jar
 done
 
+# run data generation
 for config in `ls ../eval_selection_*.properties`; do
   echo java -cp ../../bin/$path de.uni_koblenz.west.federation.test.eval.SourceSelectionEval $config
-  java -cp ../../bin/$path de.uni_koblenz.west.federation.test.eval.SourceSelectionEval $config
+#  java -cp ../../bin/$path de.uni_koblenz.west.federation.test.eval.SourceSelectionEval $config
 done 
 
-
+# generate charts from data using gnuplot
 cd plots
 gnuplot source-selection_number-of-sources.plt \
         source-selection_requests-fragments.plt \
         source-selection_requests-patterns.plt
+cd ..
+
+## prevent gs (when called by epstopdf) from eager page rotation
+export GS_OPTIONS="-dAutoRotatePages=/All"
+
+# convert ps to pdf
+cd charts
+for ps in `ls *.ps`; do
+#  echo $ps -> ${ps%.*}.pdf
+  ps2pdf -dEPSCrop $ps
+#  epstopdf $ps # does not work correctly: wront rotation
+  pdfcrop --noverbose --margins 10 ${ps%.*}.pdf ${ps%.*}.pdf
+done
+rm *.ps
 cd ..
 
