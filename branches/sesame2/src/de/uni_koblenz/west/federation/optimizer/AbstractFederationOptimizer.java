bdc0cfd172997c209fedfb129afd0ed6e9cbad98
olaf.goerlitz <olaf.goerlitz@d23ae19b-2206-6962-c3d4-7c8097259e2e>
2011-06-19 18:18:44 +0000

    new generation of base relations

--- branches/sesame2/src/de/uni_koblenz/west/federation/optimizer/AbstractFederationOptimizer.java
+++ branches/sesame2/src/de/uni_koblenz/west/federation/optimizer/AbstractFederationOptimizer.java
@@ -20,16 +20,29 @@
  */
 package de.uni_koblenz.west.federation.optimizer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.Dataset;
+import org.openrdf.query.algebra.Filter;
+import org.openrdf.query.algebra.Join;
 import org.openrdf.query.algebra.StatementPattern;
 import org.openrdf.query.algebra.TupleExpr;
+import org.openrdf.query.algebra.ValueExpr;
 import org.openrdf.query.algebra.evaluation.QueryOptimizer;
+import org.openrdf.query.algebra.helpers.StatementPatternCollector;
+import org.openrdf.query.algebra.helpers.VarNameCollector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.uni_koblenz.west.federation.helpers.FilterConditionCollector;
 import de.uni_koblenz.west.federation.helpers.OperatorTreePrinter;
 import de.uni_koblenz.west.federation.model.BasicGraphPatternExtractor;
+import de.uni_koblenz.west.federation.model.MappedStatementPattern;
+import de.uni_koblenz.west.federation.model.PatternGroupBuilder;
+import de.uni_koblenz.west.federation.sources.SourceSelector;
 
 /**
  * Base functionality for federated query optimizers
@@ -40,8 +53,56 @@ public abstract class AbstractFederationOptimizer implements QueryOptimizer {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractFederationOptimizer.class);
 	
+	protected SourceSelector selector;
+	protected PatternGroupBuilder builder;
+	
+	public AbstractFederationOptimizer(SourceSelector selector, PatternGroupBuilder builder) {
+		if (selector == null)
+			throw new IllegalArgumentException("source selector must not be null");
+		if (builder == null)
+			throw new IllegalArgumentException("pattern group builder must not be null");
+		
+		this.selector = selector;
+		this.builder = builder;
+	}
+	
 	public abstract void optimizeBGP(TupleExpr query);
-
+	
+	protected List<TupleExpr> getBaseExpressions(TupleExpr expr) {
+		
+		List<TupleExpr> baseExpressions = new ArrayList<TupleExpr>();
+		
+		// get patterns and filter conditions from query model
+		List<StatementPattern> patterns = StatementPatternCollector.process(expr);
+		List<ValueExpr> conditions = FilterConditionCollector.process(expr);
+		
+		// create patterns with source mappings and group them
+		List<MappedStatementPattern> mappedPatterns = this.selector.mapSources(patterns);
+		List<List<MappedStatementPattern>> groups = this.builder.getGroups(mappedPatterns);
+		
+		// create list of base expressions from all statement pattern lists
+		for (List<MappedStatementPattern> pList : groups) {
+			TupleExpr baseExpr = null;
+			
+			// join all statements of a list
+			for (MappedStatementPattern pattern : pList) {
+				baseExpr = (baseExpr == null) ? pattern : new Join(baseExpr, pattern);
+			}
+			
+			// add all applicable filters
+			Set<String> varNames = VarNameCollector.process(baseExpr);
+			for (ValueExpr condition : conditions) {
+				if (varNames.containsAll(VarNameCollector.process(condition))) {
+					baseExpr = new Filter(baseExpr, condition);
+				}
+			}
+			
+			baseExpressions.add(baseExpr);
+		}
+		
+		return baseExpressions;
+	}
+	
 	@Override
 	public void optimize(TupleExpr query, Dataset dataset, BindingSet bindings) {  // Sesame 2
 		
@@ -49,13 +110,14 @@ public abstract class AbstractFederationOptimizer implements QueryOptimizer {
 		for (TupleExpr bgp : BasicGraphPatternExtractor.process(query)) {
 			
 			if (LOGGER.isDebugEnabled())
-				LOGGER.warn("BGP:\n" + OperatorTreePrinter.print(bgp));	
+				LOGGER.debug("processing BGP:\n" + OperatorTreePrinter.print(bgp));
 			
-			// select sources for basic graph pattern
-
-			// a single statement pattern needs no optimization
-			if (bgp instanceof StatementPattern)
-				continue;
+//			// TODO: select sources by replacing the patterns in the tree
+//
+//			// a single statement pattern needs no optimization
+//			// TODO: need sources first
+//			if (bgp instanceof StatementPattern)
+//				continue;
 
 			optimizeBGP(bgp);
 		}
