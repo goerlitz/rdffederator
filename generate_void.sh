d35f47978d91cf490055f29282f6670d7e0f0cb2
goerlitz <olaf.goerlitz@gmail.com>
2011-07-01 20:32:52 +0000

    added distinct subject and object per predicate to void generation script

--- generate_void.sh
+++ generate_void.sh
@@ -32,20 +32,24 @@ echo >>$voidfile "[] a void:Dataset ;"
 # define arrays for the properties and types
 declare -a props
 declare -a types
+declare -a distp
 
 start=$(date +%s)
 
 echo "counting triples and properties"
 props=($(awk '{ arr[$2]++ } END { OFS="\t"; for(no in arr) { print arr[no], no } }' $ntriples | sort -k2))
 let "count = ${#props[*]} / 2"
-for ((  i = 0 ;  i < $count;  i++  )); do
+for (( i = 0 ;  i < $count;  i++ )); do
     echo >>$voidfile -e "\t"`[ $i = 0 ] && echo "void:propertyPartition [" || echo "] , ["`
     echo >>$voidfile -e "\t\tvoid:property ${props[$i*2+1]} ;"
     echo >>$voidfile -e "\t\tvoid:triples \"${props[$i*2]}\" ;"
+
+    distp=($(grep ${props[$i*2+1]} $ntriples | awk '{ FS="[ ]"; s_arr[$1]++ ; line=""; for (i=3;i<=NF-1;i++) line=line $i" "; o_arr[line]++ } END { OFS="\t"; for(no in s_arr) { s_count++ } for(no in o_arr) { o_count++ } print s_count, o_count }'))
+    echo >>$voidfile -e "\t\tvoid:distinctSubjects \"${distp[0]}\" ;"
+    echo >>$voidfile -e "\t\tvoid:distinctObjects \"${distp[1]}\" ;"
     let "triple_count += ${props[$i*2]}"
 done
 echo >>$voidfile -e "\t] ;"
-
 
 echo "counting entities and types"
 types=($(grep '#type' $ntriples | awk '{ arr[$3]++ } END { OFS="\t"; for(no in arr) { print arr[no], no } }' | sort -k2))
@@ -62,8 +66,7 @@ echo >>$voidfile -e "\t] ;"
 echo "counting distinct subjects"
 distinct_sub=$(awk '{ arr[$1]++ } END { for(no in arr) { print no } }' $ntriples | wc -l)
 echo "counting distinct objects" # field separator is single space: FS="[ ]"
-distinct_obj=$(awk '{ FS="[ ]"; ORS=""; line=""; for (i=3;i<=NF-1;i++) line=line $i" "; arr[line"\n"]++} END { for(no in arr) { print no } }' $ntriples | wc -l)
-
+distinct_obj=$(awk '{ FS="[ ]"; line=""; for (i=3;i<=NF-1;i++) line=line $i" "; arr[line]++} END { for(no in arr) { print no } }' $ntriples | wc -l)
 
 # print general statistics
 echo >>$voidfile -e "\tvoid:triples \"$triple_count\" ;"
