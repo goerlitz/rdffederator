c2db50cd12c4fb2f9f7f2eca98697f051bcfede9
goerlitz <olaf.goerlitz@gmail.com>
2011-08-03 14:08:26 +0000

    improved void generation script using awk

--- generate_void.sh
+++ generate_void.sh
@@ -1,5 +1,5 @@
 # !/bin/sh
-# generates void descriptions from ntriple files.
+# generates void statistics from ntriple files.
 
 # check script arguments
 # if [ $# = 0 ]; then
@@ -17,7 +17,7 @@ if [ ! -e $ntriples ]; then
     exit;
 fi
 
-# check existence of output file: will not overwrite
+# check existence of output file, do not overwrite
 if [ -e $voidfile ]; then
     echo "ERROR: VOID output file '$voidfile' already exist";
     # TODO: ask for permission to overwrite
@@ -29,52 +29,54 @@ echo  >$voidfile "@prefix void: <http://rdfs.org/ns/void#> ."
 echo >>$voidfile ""
 echo >>$voidfile "[] a void:Dataset ;"
 
-# define arrays for the properties and types
-declare -a props
-declare -a types
-declare -a distp
+# define arrays for the properties, types, and distinct subjects/objects
+declare -a props types dists disto
 
 start=$(date +%s)
 
 echo "counting triples and properties"
 props=($(awk '{ arr[$2]++ } END { OFS="\t"; for(no in arr) { print arr[no], no } }' $ntriples | sort -k2))
-let "count = ${#props[*]} / 2"
-for (( i = 0 ;  i < $count;  i++ )); do
-    echo >>$voidfile -e "\t"`[ $i = 0 ] && echo "void:propertyPartition [" || echo "] , ["`
-    echo >>$voidfile -e "\t\tvoid:property ${props[$i*2+1]} ;"
-    echo >>$voidfile -e "\t\tvoid:triples \"${props[$i*2]}\" ;"
 
-    distp=($(grep ${props[$i*2+1]} $ntriples | awk '{ FS="[ ]"; s_arr[$1]++ ; line=""; for (i=3;i<=NF-1;i++) line=line $i" "; o_arr[line]++ } END { OFS="\t"; for(no in s_arr) { s_count++ } for(no in o_arr) { o_count++ } print s_count, o_count }'))
-    echo >>$voidfile -e "\t\tvoid:distinctSubjects \"${distp[0]}\" ;"
-    echo >>$voidfile -e "\t\tvoid:distinctObjects \"${distp[1]}\" ;"
-    let "triple_count += ${props[$i*2]}"
-done
-echo >>$voidfile -e "\t] ;"
-
-echo "counting entities and types"
+echo "counting types and entities"
 types=($(grep '#type' $ntriples | awk '{ arr[$3]++ } END { OFS="\t"; for(no in arr) { print arr[no], no } }' | sort -k2))
-let "count = ${#types[*]} / 2"
-for ((  i = 0 ;  i < $count;  i++  )); do
-    echo >>$voidfile -e "\t"`[ $i = 0 ] && echo "void:classPartition [" || echo "] , ["`
-    echo >>$voidfile -e "\t\tvoid:class ${types[$i*2+1]} ;"
-    echo >>$voidfile -e "\t\tvoid:entities \"${types[$i*2]}\" ;"
-    let "class_count += ${types[$i*2]}"
-done
-echo >>$voidfile -e "\t] ;"
 
+echo "counting distinct objects"
+disto=($(./count_predicate_distinct_o $ntriples | sort))
 
 echo "counting distinct subjects"
-distinct_sub=$(awk '{ arr[$1]++ } END { for(no in arr) { print no } }' $ntriples | wc -l)
-echo "counting distinct objects" # field separator is single space: FS="[ ]"
-distinct_obj=$(awk '{ FS="[ ]"; line=""; for (i=3;i<=NF-1;i++) line=line $i" "; arr[line]++} END { for(no in arr) { print no } }' $ntriples | wc -l)
+dists=($(./count_predicate_distinct_s $ntriples | sort))
+
+# collect numbers of general statistics
+let "p_count = ${#props[*]} / 2"
+let "t_count = ${#types[*]} / 2"
+let "s_count = ${dists[$p_count*2+1]}"
+let "o_count = ${disto[$p_count*2+1]}"
+for (( i = 0 ;  i < $p_count;  i++ )); do let "triple_count += ${props[$i*2]}"; done
 
 # print general statistics
 echo >>$voidfile -e "\tvoid:triples \"$triple_count\" ;"
-echo >>$voidfile -e "\tvoid:properties \"$((${#props[*]} / 2))\" ;"
-echo >>$voidfile -e "\tvoid:classes \"$((${#types[*]} / 2))\" ;"
-echo >>$voidfile -e "\tvoid:entities \"$class_count\" ;"
-echo >>$voidfile -e "\tvoid:distinctSubjects \"$distinct_sub\" ;"
-echo >>$voidfile -e "\tvoid:distinctObjects \"$distinct_obj\" ."
+echo >>$voidfile -e "\tvoid:classes \"$t_count\" ;"
+echo >>$voidfile -e "\tvoid:properties \"$p_count\" ;"
+echo >>$voidfile -e "\tvoid:distinctSubjects \"$s_count\" ;"
+echo >>$voidfile -e "\tvoid:distinctObjects \"$o_count\" ;"
+
+# print predicate statistics
+for (( i = 0 ;  i < $p_count;  i++ )); do
+    echo >>$voidfile -e "\t"`[ $i = 0 ] && echo "void:propertyPartition [" || echo "] , ["`
+    echo >>$voidfile -e "\t\tvoid:property ${props[$i*2+1]} ;"
+    echo >>$voidfile -e "\t\tvoid:triples \"${props[$i*2]}\" ;"
+    echo >>$voidfile -e "\t\tvoid:distinctSubjects \"${dists[$i*2+1]}\" ;"
+    echo >>$voidfile -e "\t\tvoid:distinctObjects \"${disto[$i*2+1]}\" ;"
+done
+echo >>$voidfile -e "\t] ;"
+
+# print type statistics
+for (( i = 0 ;  i < $t_count;  i++ )); do
+    echo >>$voidfile -e "\t"`[ $i = 0 ] && echo "void:classPartition [" || echo "] , ["`
+    echo >>$voidfile -e "\t\tvoid:class ${types[$i*2+1]} ;"
+    echo >>$voidfile -e "\t\tvoid:entities \"${types[$i*2]}\" ;"
+done
+echo >>$voidfile -e "\t] ."
 
 let "time=$(date +%s)-$start"
 echo "time taken: $time seconds"
