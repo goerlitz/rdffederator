df6c4a4f3a324bfd9729f7646a1dc8daaf702690
olaf.goerlitz <olaf.goerlitz@d23ae19b-2206-6962-c3d4-7c8097259e2e>
2011-08-03 15:07:45 +0000

    run scripts updated

--- branches/sesame2/run_voidgen.sh
+++ branches/sesame2/run_voidgen.sh
@@ -1,16 +1,15 @@
 # !/bin/sh
 
-mainclass=de.uni_koblenz.west.statistics.void2.Void2StatisticsGenerator
+#mainclass=de.uni_koblenz.west.statistics.void2.Void2StatisticsGenerator
+mainclass=de.uni_koblenz.west.statistics.util.VoidGenerator
 
 # collect all jars in lib/ for inclusion in the classpath
-for jar in lib/*.jar; do
-  classpath=$classpath:$jar
-done
+for jar in lib/*.jar; do classpath=$classpath:$jar; done
 
 # add current dir and bin/ dir to classpath
-classpath=.:bin/:$classpath
+classpath=.:bin/$classpath
 
 # run main class with classpath setting
 #echo java -cp $classpath $mainclass $*
-java -cp $classpath $mainclass $*
+java -cp $classpath -Xmx2g $mainclass $* 1>void.n3
 
