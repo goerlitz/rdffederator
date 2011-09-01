bfeff0ee4d8282661ac57fa5de8072ed5693e0a7
olaf.goerlitz <olaf.goerlitz@d23ae19b-2206-6962-c3d4-7c8097259e2e>
2011-09-01 14:02:34 +0000

    fixed error handling in structuredness analyzer and added start script

---
+++ branches/sesame2/analyze_structuredness.sh
@@ -0,0 +1,13 @@
+# !/bin/sh
+
+mainclass=de.uni_koblenz.west.statistics.util.StructurednessAnalyzer
+
+# add current dir and bin/ to the classpath
+classpath=.:bin/
+
+# add all jars in lib/ to the classpath
+for jar in lib/*.jar; do classpath=$classpath:$jar; done
+
+# run main class with classpath setting
+java -cp $classpath -Xmx2g $mainclass $*
+
