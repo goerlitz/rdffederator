d9c5c30fcfe5450cdaebb60f9ca40968158760c2
goerlitz <olaf.goerlitz@gmail.com>
2011-09-08 14:03:23 +0000

    fixed handling of N-Triple URIs containing whitespace in darq generator

--- scripts/generate_darq_description.sh
+++ scripts/generate_darq_description.sh
@@ -1,8 +1,14 @@
 # !/bin/sh
 #
 # Generator for DARQ service descriptions.
+#
 # Input:  RDF graph in N-Triples format (one triple per line).
 # Output: DARQ service description in N3 format.
+#
+# Features: correct handling of URIs containing whitespaces
+#           ignores blank lines and comment lines starting with '#'
+#
+# Author: goerlitz@uni-koblenz.de
 #####################################################################
 
 # use handy variable names for arguments
@@ -22,36 +28,40 @@ if [ -e $statfile ]; then
     exit;
 fi
 
-start=$(date +%s)
+start=$(date +%s)  # measure overall processing time
 
-echo "counting triples, properties, and types"
-#declare -a map_pred_type
+# use awk to count triples and number of occurrences of predicates and types
+# filters blank and comment lines with sed (faster than 'NF && !/^#/' in awk)
+IFS=$'\t\n'  # prevents splitting of awk output on ' ' (in URIs)
+declare -a map_pred_type=($(cat $ntriples | sed '/^ *#/d;/^ *$/d' | awk '{
+  FS="[ ]"  # prevents merging of consecutive whitespaces
+  s=$1; p=$2; o=""
 
-# count number of occurrences of predicates and types in input file
-# ignore blank and comment lines (sed is faster than awk)
-#map_pred_type=($(cat $1 | awk 'NF && ($1 !~ "^#") {
-map_pred_type=($(cat $ntriples | sed '/^ *#/d;/^ *$/d' | awk '{
-  s=$1; p=$2; o=$3
-
-  # check URIs (regular splitting fails for URIs with whitespace)
-  if ( ! (p ~ /^</ && p ~ />$/) ) {  # slow, but faster than /^<.*>$/
+  # check URIs (regular splitting breaks URIs with whitespaces)
+  if ( !(p ~ /^</ && p ~ />$/) ) {  # slow, but faster than /^<.*>$/
     # do splitting again taking spaces in URIs into account
     col=0;
     s=$++col; while (s ~ /^</ && ! (s ~ />$/)) s=s" "$++col
     p=$++col; while (p ~ /^</ && ! (p ~ />$/)) p=p" "$++col
     o=$++col; for (i=++col;i<=NF-1;i++) o=o" "$i
+  } else {
+    o=$3; for (i=4;i<=NF-1;i++) o=o" "$i
   }
 
   # count predicates and types
   pred[p]++
   if (p == "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>") type[o]++
-}
-END {
+} END {
+#  OFS=""; for (no in pred) print "DEBUG: ",pred[no]," -> P:"no >"/dev/stderr"  # DEBUGGING
+
   OFS="\t";
   # print count and URI of predicate/type with prefix 'P:' or 'T:'
   for (no in pred) print pred[no],"P:"no
   for (no in type) print type[no],"T:"no
 }' | sort -k2))
+unset IFS  # restore default field seperators (' \t \n')
+#echo ${#map_pred_type[*]} " elements"
+#for (( i=0 ;  i<${#map_pred_type[*]} ;  i++ )); do echo "KEY=${map_pred_type[i]}, VAL=${map_pred_type[++i]}"; done
 
 # count triples, predicates, and types
 let "map_count = ${#map_pred_type[*]} / 2"
@@ -65,8 +75,7 @@ for (( i = 0 ;  i < $map_count;  i++ )); do
   fi
 done
 
-# create DARQ regex for all types
-# regex="REGEX(STR(?object),'${types[1]}')"
+# create DARQ regex string for all types
 for (( i = $pred_count ;  i < $map_count;  i++ )); do
   if [ "$regex" != "" ]; then regex=$regex" || "; fi
   type=${map_pred_type[$i*2+1]}
