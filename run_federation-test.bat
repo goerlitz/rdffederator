90f4fdfede740487585891aaa7846200a1a7d2d8
goerlitz <olaf.goerlitz@gmail.com>
2011-09-21 17:33:11 +0000

    moved start scripts

--- run_federation-test.bat
+++ run_federation-test.bat
@@ -3,6 +3,8 @@ REM run SPLENDID federation test
 
 set MAIN=de.uni_koblenz.west.federation.test.OptimizerTest
 
+cd eval
+
 REM set classpath
 set CP=..\bin
 
@@ -10,6 +12,7 @@ REM include jar files in classpath
 for %%i in (..\lib\*.jar) do CALL :SETTER %%i
 
 java -cp %CP% %MAIN% %*
+cd ..
 goto :eof
 
 :SETTER
